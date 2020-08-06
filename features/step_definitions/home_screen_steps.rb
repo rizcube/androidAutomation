86f9ccec827741d934544234bfca705689b69e18
Rizwan Ali <rizcubeservices@gmail.com>
2020-08-06 14:06:00 +0100

    added parameter without using quotes - Then(/^Show All button should be (enabled|disabled)$/) do |state|

--- features/step_definitions/home_screen_steps.rb
+++ features/step_definitions/home_screen_steps.rb
@@ -6,14 +6,10 @@ Then('Right unit picker value should be {string}') do |value|
   log("Right unit picker value is "+ value)
 end
 
-Then('Show All button should be enabled') do
+Then(/^Show All button should be (enabled|disabled)$/) do |state|
   log("button is enabled")
 end
 
 When('I press on Clear button') do
   log("clear button is pressed")
-end
-
-Then('Show All button should be disabled') do
-  log("button is disabled")
 end
