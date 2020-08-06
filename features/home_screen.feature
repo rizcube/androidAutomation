59d538c1e1eac963d3087f39c80fea906a845ab5
Rizwan Ali <rizcubeservices@gmail.com>
2020-08-06 13:54:25 +0100

    Scenario: included with step definition Show All button should be enabled at launch

--- features/home_screen.feature
+++ features/home_screen.feature
@@ -1,5 +1,12 @@
 Feature: Tests for Home screen functionality
   Scenario: Default values on Home screen is Foot and Centimeter
     Given I land on home screen
-    Then Left unit picker value should be Foot
-    And Right unit picker value should be Centimeter
+    Then Left unit picker value should be "Foot"
+    Then Left unit picker value should be "Inch"
+    And Right unit picker value should be "Centimeter"
+
+    Scenario: Show All button should be enabled at launch
+      Given I land on home screen
+      Then Show All button should be enabled
+      When I press on Clear button
+      Then Show All button should be disabled
--- features/step_definitions/home_screen_steps.rb
+++ features/step_definitions/home_screen_steps.rb
@@ -1,7 +1,19 @@
-Then('Left unit picker value should be Foot') do
-  log("Left unit picker value is foot")
+Then('Left unit picker value should be {string}') do |value|
+  log("Left unit picker value is "+ value)
 end
 
-Then('Right unit picker value should be Centimeter') do
-  log("Right unit picker value is centimeter")
+Then('Right unit picker value should be {string}') do |value|
+  log("Right unit picker value is "+ value)
+end
+
+Then('Show All button should be enabled') do
+  log("button is enabled")
+end
+
+When('I press on Clear button') do
+  log("clear button is pressed")
+end
+
+Then('Show All button should be disabled') do
+  log("button is disabled")
 end
