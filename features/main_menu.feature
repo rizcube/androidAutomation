2e9aa85cb1bd3047226bd4986fcf836689f4225b
Rizwan Ali <rizcubeservices@gmail.com>
2020-08-06 12:50:59 +0100

    added step definition for first scenario

---
+++ features/main_menu.feature
@@ -0,0 +1,5 @@
+Feature: As a user I want to convert units
+  Scenario: When I tap on menu icon, i should see left side menu
+    Given I land on home screen
+    When I press on menu icon
+    Then I should see left side menu
---
+++ features/step_definitions/main_menu_steps.rb
@@ -0,0 +1,12 @@
+
+Given('I land on home screen') do
+  log("Landed on home screen")
+end
+
+When('I press on menu icon') do
+  log("Menu icon pressed")
+end
+
+Then('I should see left side menu') do
+  log("I see left side menu")
+end
